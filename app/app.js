3e6352721e59a28b07a274683998fe7ffa3c85bd
Valentyn Riabokonenko <Valentyn_Riabokonenko@epam.com>
2016-07-14 15:58:39 +0200

    Finishing the example, testing features

--- app/app.js
+++ app/app.js
@@ -2,18 +2,79 @@
     "use strinct";
     angular.module("app", [])
             .controller("Todo", Todo)
+            .factory("todoService", todoService)
+            .filter("checkedItems", checkedItems)
+            .run(runApp)
+            .directive("taskList", taskList)
             .value("model",{
               user: "Vitaliy",
               userPhoto: "images/VZ.jpg",
-              items: [
-                    { "action": "Estimate...", "done": false},
-                    { "action": "Create...", "done": false},
-                    { "action": "Edit...", "done": true},
-                    { "action": "Delete...", "done": false},
-                ],
             });
-    function Todo($scope, model) {
+      // controller
+    function Todo($scope, model, todoService) {
         $scope.todo = model;
-        console.log($scope.todo);
+        $scope.addNewItem = todoService.addNewItem;
+        $scope.incompleteCount = todoService.incompleteCount;
+        $scope.warningLevel = todoService.warningLevel;
     }
+
+    // factory
+    function todoService() {
+      function addNewItem(items, newItem){
+        if(newItem && newItem.action){
+          items.push({
+            action: newItem.action,
+            done: false
+          });
+          newItem.action = "";
+        }
+      };
+        function incompleteCount (items) {
+            let count = 0;
+
+            angular.forEach(items, (item) => {
+              if (!item.done) count++;
+            });
+          return count;
+        };
+
+        function warningLevel(items){
+          return incompleteCount(items) < 3 ? "label-success" : "label-warning"
+        };
+
+        return {
+            incompleteCount,
+            warningLevel,
+            addNewItem
+          };
+      }
+
+      // filter
+      function checkedItems(){
+        return function(items, showComplete){
+          let resArr = [];
+
+          angular.forEach(items, (item) => {
+            if(!item.done || showComplete){
+              resArr.push(item);
+            }
+          });
+          return resArr;
+        }
+      };
+
+      //run
+      function runApp($http, model){
+        $http
+            .get("todo.json")
+            .then((response) =>{
+              model.items = response.data;
+            })
+      };
+
+      function taskList(){
+        return{
+          templateUrl: "table.html"
+        }
+      };
 })();
